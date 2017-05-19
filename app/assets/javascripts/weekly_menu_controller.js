59c41b4091d6a1e2743dcea068a9a825444a8bc0
Michael <michael.ferretti@outlook.com>
2017-05-18 20:50:58 -0400

    Add Weeklymenu click function

---
+++ app/assets/javascripts/weekly_menu_controller.js
@@ -0,0 +1,12 @@
+(function () {
+	"use strict";
+
+	angular.module("app").controller("weeklyMenuController", function($scope) {
+		$scope.currentRecipeUrl = "";
+		$scope.changeWeeklyRecipe = function(recipeUrl) {
+			
+		};
+
+	});
+
+})();
