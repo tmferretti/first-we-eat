73a9e886916372c46021ae58a4a5a59b53a73189
Michael <michael.ferretti@outlook.com>
2017-05-18 21:22:48 -0400

    Complete JS weeklymenuchange function

--- app/assets/javascripts/weekly_menu_controller.js
+++ app/assets/javascripts/weekly_menu_controller.js
@@ -1,10 +1,17 @@
 (function () {
 	"use strict";
 
-	angular.module("app").controller("weeklyMenuController", function($scope) {
+	angular.module("app").controller("weeklyMenuController", function($scope, $sce) {
 		$scope.currentRecipeUrl = "";
+		
+
 		$scope.changeWeeklyRecipe = function(recipeUrl) {
-			
+			$scope.trustSrc = function(recipeUrl) {
+			    console.log(recipeUrl)
+			    return $sce.trustAsResourceUrl(recipeUrl);
+			}
+			console.log($scope.trustSrc())
+			$scope.currentRecipeUrl = $scope.trustSrc(recipeUrl);
 		};
 
 	});
